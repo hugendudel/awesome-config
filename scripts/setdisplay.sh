b39f6d7312985d81072d05de93945f5655082a5e
Harm Müller <harm.mueller@gmail.com>
2016-07-14 19:00:50 +0200

    code cleanup

--- scripts/setdisplay.sh
+++ scripts/setdisplay.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 EXT_MON=$(xrandr |awk '/ connected/ && ! /LVDS1/ {print $1}')
-[[ -n ${EXT_MON} ]] && xrandr --output $EXT_MON --right-of LVDS1 --auto || xrandr --auto
+[[ -n ${EXT_MON} ]] && xrandr --output ${EXT_MON} --right-of LVDS1 --auto || xrandr --auto
